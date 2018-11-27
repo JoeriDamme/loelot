fcb1ae449b0c2538f9797a2ff5aae905e2438f3b
Joeri Damme <joeri.damme@kpn.com>
2018-11-27 22:10:23 +0100

    feat: Sequelize database connection.

--- src/lib/passport/facebook-token-strategy.ts
+++
@@ -1,9 +0,0 @@
-import passport from 'passport';
-import Strategy, { Profile, VerifyFunction } from 'passport-facebook-token';
-
-passport.use('facebook-token', new Strategy({
-  clientID: process.env.FACEBOOK_APP_ID as string,
-  clientSecret: process.env.FACEBOOK_APP_SECRET as string,
-}, (accessToken: string, refreshToken: string, profile: Profile, done: any): VerifyFunction => {
-  return done();
-}));
---
+++ src/lib/sequelize.ts
@@ -0,0 +1,12 @@
+import { Sequelize } from 'sequelize-typescript';
+
+export default new Sequelize({
+  database: process.env.DB_NAME as string,
+  dialect: process.env.DB_DIALECT as string,
+  host: process.env.DB_HOST as string,
+  modelPaths: [`${__dirname}/../app/models/*.model.ts`],
+  operatorsAliases: Sequelize.Op as any,
+  password: process.env.DB_PASS as string,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USER as string,
+});
