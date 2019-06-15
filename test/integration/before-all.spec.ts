d708cc7ea331b24eb0dba8b616f9ceb50afbffe4
Joeri Damme <joeri.damme@kpn.com>
2019-06-15 22:52:22 +0200

    feat: removed warning operator aliases.

--- test/integration/before-all.spec.ts
+++ test/integration/before-all.spec.ts
@@ -2,29 +2,14 @@ import dotenv from 'dotenv';
 dotenv.config();
 import bluebird from 'bluebird';
 import config from 'config';
-import { Model, Op, Sequelize } from 'sequelize';
+import { Sequelize } from 'sequelize';
 import * as models from '../../src/models';
 import Role from '../../src/models/role.model';
-import User from '../../src/models/user.model';
-
-// const sequelize: Sequelize = new Sequelize({
-//   database: config.get('database.name') as string,
-//   dialect: process.env.DB_DIALECT as string,
-//   host: process.env.DB_HOST as string,
-//   logging: false,
-//   modelPaths: [`${__dirname}/../../src/models/*.model.ts`],
-//   operatorsAliases: Sequelize.Op as any,
-//   password: process.env.DB_PASS as string,
-//   port: Number(process.env.DB_PORT),
-//   timezone: '+01:00',
-//   username: process.env.DB_USER as string,
-// });
 
 const sequelize: Sequelize = new Sequelize(config.get('database.name'), process.env.DB_USER as string, process.env.DB_PASS as string, {
   dialect: 'postgres',
   host: process.env.DB_HOST as string,
   logging: false,
-  operatorsAliases: Op,
   port: Number(process.env.DB_PORT),
   timezone: '+01:00',
 });
